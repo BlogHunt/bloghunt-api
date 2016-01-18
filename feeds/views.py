41b02655110edd4487a5feb831e7e524de6886e5
Macro <macromicah@gmail.com>
2016-01-18 15:52:08 -0800

    Remove ordering

--- feeds/views.py
+++ feeds/views.py
@@ -7,8 +7,7 @@ from . import models, serializers
 
 
 class FeedViewSet(viewsets.ModelViewSet):
-    queryset = models.Feed.objects.filter(
-        last_updated__isnull=False).order_by('rss_url')
+    queryset = models.Feed.objects.filter(last_updated__isnull=False)
     serializer_class = serializers.FeedSerializer
 
     @detail_route(methods=['get'])
