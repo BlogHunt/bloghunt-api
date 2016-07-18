4051f43911dad28fe4e967c261565e7dfcb46e48
Brian Schrader <brian@brianschrader.com>
2016-07-17 18:20:49 -0700

    Fixes flake issues

--- feeds/serializers.py
+++ feeds/serializers.py
@@ -4,12 +4,11 @@ from . import models
 
 
 class FeedSerializer(serializers.HyperlinkedModelSerializer):
-    tags = serializers.HyperlinkedRelatedField('tag-detail',
-        queryset=models.Tag.objects.all(), many=True)
+    tags = serializers.HyperlinkedRelatedField('tag-detail', queryset=models.Tag.objects.all(), many=True)
 
     class Meta:
         fields = ['url', 'rss_url', 'title', 'description', 'link', 'tags',
-            'cloud', 'image']
+                  'cloud', 'image']
         model = models.Feed
         read_only_fields = ['title', 'description', 'link']
 
