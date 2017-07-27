dd0ba923506474bd1b7a466f5a1f65bd042a7a18
Brian Schrader <brian@brianschrader.com>
2017-07-26 19:39:30 -0700

    Fixes linting

--- bloghunt/settings.py
+++ bloghunt/settings.py
@@ -136,7 +136,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
-     'DEFAULT_RENDERER_CLASSES': (
+    'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.TemplateHTMLRenderer',
         'rest_framework.renderers.JSONRenderer',
     ),
